94572dfbb8fa0163abbff5278ef85abdfd6349d4
tundemy <babtunde25@gmail.com>
2024-03-02 04:39:28 -0500

    Assignment 3 now modified to Assignment 4

--- modules/collegeData.js
+++ modules/collegeData.js
@@ -39,6 +39,53 @@ module.exports.getAllStudents = function () {
         }
 
         resolve(dataCollection.students);
+    })
+}
+
+module.exports.getTAs = function () {
+    return new Promise(function (resolve, reject) {
+        var filteredStudents = [];
+
+        for (let i = 0; i < dataCollection.students.length; i++) {
+            if (dataCollection.students[i].TA == true) {
+                filteredStudents.push(dataCollection.students[i]);
+            }
+        }
+
+        if (filteredStudents.length == 0) {
+            reject("query returned 0 results"); return;
+        }
+
+        resolve(filteredStudents);
+    });
+};
+
+module.exports.getCourses = function(){
+   return new Promise((resolve,reject)=>{
+    if (dataCollection.courses.length == 0) {
+        reject("query returned 0 results"); return;
+    }
+
+    resolve(dataCollection.courses);
+   });
+};
+
+module.exports.getStudentByNum = function (num) {
+    return new Promise(function (resolve, reject) {
+        var foundStudent = null;
+
+        for (let i = 0; i < dataCollection.students.length; i++) {
+            if (dataCollection.students[i].studentNum == num) {
+                foundStudent = dataCollection.students[i];
+                break;
+            }
+        }
+
+        if (!foundStudent) {
+            reject("no results returned"); return;
+        }
+
+        resolve(foundStudent);
     });
 }
 
@@ -50,6 +97,35 @@ module.exports.addStudent = function (studentData) {
         }
 
         // Add logic to add student to dataCollection.students array
+        module.exports.addStudent = function (studentData) {
+            return new Promise((resolve, reject) => {
+                if (!studentData) {
+                    reject("No student data provided");
+                    return;
+                }
+        
+                // Generate a unique student number
+                const studentNum = dataCollection.students.length + 1;
+        
+                // Create a new student object
+                const newStudent = {
+                    studentNum: studentNum,
+                    firstName: studentData.firstName,
+                    lastName: studentData.lastName,
+                    email: studentData.email,
+                    GPA: studentData.gpa,
+                    course: studentData.course,
+                    TA: studentData.ta === "on" ? true : false // Convert "on" to boolean
+                };
+        
+                // Add the new student to the dataCollection.students array
+                dataCollection.students.push(newStudent);
+        
+                // Resolve the promise
+                resolve();
+            });
+        }
+        
         // Resolve or reject the promise based on the success or failure of the operation
     });
 }
